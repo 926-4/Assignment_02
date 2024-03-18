2643a9d803bf7ef511febf6ce9bae96392d15c6a
Sirbu Ciprian <ciprisbu21@gmail.com>
2024-03-18 13:31:38 +0200

    Testing

---
+++ Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using Logging;
+using DataValidation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LoggingModule a = new LoggingModule();
+        Validator b = new Validator();
+        a.LoggingMethod();
+        b.Method();
+        Console.WriteLine("Press any key to finish...");
+        Console.ReadLine();
+    }
+}
