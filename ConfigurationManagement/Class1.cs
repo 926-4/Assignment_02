79c60b3d5ff2d3b705e3efab783cb40feb9edb75
Sirbu Ciprian <ciprisbu21@gmail.com>
2024-03-19 16:02:24 +0200

    ConfigManagement

---
+++ ConfigurationManagement/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ConfigurationManagement
+{
+    public class ConfigMngModule
+    {
+        public void ConfigMethod()
+        {
+            Console.WriteLine("Eu sunt Ioan :)");
+        }
+    }
+}
