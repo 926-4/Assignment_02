57079ddcdbee5cda84039da82451f261709470d7
Sirbu Ciprian <ciprisbu21@gmail.com>
2024-03-19 15:58:26 +0200

    Authentification

---
+++ Authentification/Authentification.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentification
+{
+    public class AuthentificationModule
+    {
+        public void AuthMethod()
+        {
+            Console.WriteLine("Eu sunt Norbi");
+        }
+    }
+}
