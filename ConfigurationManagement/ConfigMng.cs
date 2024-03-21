00ff9247cfdaf0216705d5b6e41b6c3006354f0e
neon1024 <robertscumnpu@gmail.com>
2024-03-21 16:25:21 +0200

    Constructors

--- ConfigurationManagement/ConfigMng.cs
+++ ConfigurationManagement/ConfigMng.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text.Json;
 
 namespace ConfigurationManagement
 {
@@ -16,19 +18,26 @@ namespace ConfigurationManagement
         public int age { get; set; }
         public string mail { get; set; }
         public string phone_number { get; set; }
+    
+        public Profile(string handle="@user", string username="username", string bio="none", int age=0, string mail="user@mail", string phone="0000000000") { }
     }
 
     public class Privacy
     {
+        public Privacy(string profile_type = "public", bool allow_tags = true, bool allow_message = true) { }
+        
         public string profile_type { get; set; }
         public bool allow_tags { get; set; }
         public bool allow_message { get; set; }
+
     }
 
     public class Notifications
     {
         public bool push { get; set; }
         public bool mail { get; set; }
+
+        public Notifications(bool push=true, bool mail=true) { }
     }
 
     public class UserSettings
@@ -37,18 +46,23 @@ namespace ConfigurationManagement
         public Privacy privacy { get; set; }
         public Notifications notifications { get; set; }
 
+        public UserSettings(Profile profile, Privacy privacy, Notifications notifications) { }
     }
 
     public class Appearance
     {
         public string mode { get; set; }
         public string theme { get; set; }
+
+        public Appearance(string mode="system", string theme="default") { }
     }
 
     public class AdvancedSettings
     {
         public bool two_factor_authentification { get; set; }
         public List<User> blocked_users { get; set; }
+
+        public AdvancedSettings(bool two_factor_authentification = false, List<User> blocked_users = []) { }
     }
 
     public class Configuration
@@ -56,12 +70,16 @@ namespace ConfigurationManagement
         public UserSettings user_settings { get; set; }
         public Appearance appearance { get; set; }
         public string language { get; set; }
+
+        public Configuration(UserSettings user_settings, Appearance appearance, string languag = "en-US") { }
     }
 
     public class ConfigMngModule
     {
         private string ConfigurationFileName;
         private string JsonString;
+
+        public ConfigMngModule(string ConfigurationFileName = "config.json", string JsonString = "") { }
 
         public void ConfigMethod()
         {
